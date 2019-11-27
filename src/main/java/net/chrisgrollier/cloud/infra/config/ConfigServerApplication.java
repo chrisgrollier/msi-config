007dc7df9a38c859894cee109fd95f903145add5
chrisgrollier <cgrollier@free.fr>
2019-11-27 22:19:28 +0100

    Added @RefreshScope

--- src/main/java/net/chrisgrollier/cloud/infra/config/ConfigServerApplication.java
+++ src/main/java/net/chrisgrollier/cloud/infra/config/ConfigServerApplication.java
@@ -4,16 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
- * Main class of the spring-cloud-config-server app.
- * @author Atos
- *
+ * Main class.
+ * @author Christophe Grollier
  */
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer
+@RefreshScope
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
