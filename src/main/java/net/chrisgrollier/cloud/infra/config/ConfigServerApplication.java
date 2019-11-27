ebcb400a8f6d7a7460e92bd682413827b2d48641
chrisgrollier <cgrollier@free.fr>
2019-11-27 19:28:18 +0100

    Added source

---
+++ src/main/java/net/chrisgrollier/cloud/infra/config/ConfigServerApplication.java
@@ -0,0 +1,23 @@
+package net.chrisgrollier.cloud.infra.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Main class of the spring-cloud-config-server app.
+ * @author Atos
+ *
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
